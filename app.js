34673cc4bca1d3fc0a64d15a86b7ebb80263cda7
Christopher Bennage <christopher@bennage.com>
2012-03-05 22:54:53 -0800

    ignoring some well known files

--- app.js
+++ app.js
@@ -67,10 +67,17 @@ viewHelper.initialize(app);
 var fs = require('fs');
 var markdown = require("markdown").markdown;
 var files = fs.readdirSync('./source');
+var ignore = ['Copyright.markdown','README.markdown'];
+
+function shouldIgnore(file){
+
+
+}
 
 files.forEach(function(file) {
     if(file.indexOf('.markdown') === -1) return;
-    
+    if(ignore.indexOf(file) !== -1) return;
+
     var input = fs.readFileSync('./source/' + file, 'utf8');
     var output = markdown.toHTML(input);
     fs.writeFileSync('./source/' + file.replace('.markdown', '.html'), output);
