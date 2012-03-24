13dbb8d24c123f2dac2aa0381cb8572358173b93
Christopher Bennage <christopher@bennage.com>
2012-03-23 17:27:44 -0700

    remove old test code

--- convert.tests.js
+++
@@ -1,6 +0,0 @@
-var convert = require('./lib/convert')(),
-	config = require('./lib/config');
-
-var imageRoot = 'https://raw.github.com/mspnp/cqrs-journey-doc/master/';
-
-convert('./source', './markdown', imageRoot);
--- git.test.js
+++
@@ -1,6 +0,0 @@
-var github = require('./lib/github')(),
-	config = require('./lib/config');
-
-github(config.source.repo, './source/', config.source.ignore, function(err, sha) {
-	console.log(sha);
-});
