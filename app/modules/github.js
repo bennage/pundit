b9c146576c8d3c460ec3a41f0c93236fef6cd9f0
Christopher Bennage <christopher@bennage.com>
2015-01-25 18:26:18 -0800

    extracting out some bits that will be the same across GitHub API calls (premature?)

--- app/modules/github.js
+++ app/modules/github.js
@@ -1,13 +1,18 @@
 'use strict';
 define(['knockout', 'durandal/system', 'plugins/http', './node'], function(ko, system, http, Node) {
 
+    var base_url = 'https://api.github.com/';
+    var base_headers = {
+      'Accept' : 'application/vnd.github.v3+json'
+      };
+    
     function Github() {}
 
     Github.prototype.fetchRepository = function(owner, repo) {
-        var url ='https://api.github.com/';
-        url += "repos/" + owner + "/" + repo + "/git/trees/master?recursive=1";
 
-        return http.get(url, null, { 'Accept' : 'application/vnd.github.v3+json' }).then(function(response) {
+        var url = base_url + "repos/" + owner + "/" + repo + "/git/trees/master?recursive=1";
+
+        return http.get(url, null, base_headers).then(function(response) {
             if(response.truncated) { /* signal error condition */ }
             var root = Node.unflatten(response.tree);
             root.sha = response.sha;
