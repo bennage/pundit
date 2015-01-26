c1904a229d1ec3cdbc5f89cbf64f4da84e6d5407
Christopher Bennage <christopher@bennage.com>
2015-01-25 22:55:25 -0800

    retrieving and displaying a raw blob

--- app/viewmodels/document.js
+++ app/viewmodels/document.js
@@ -1,13 +1,22 @@
-define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
+define(['plugins/http', 'durandal/app', 'knockout', '../modules/github'], function (http, app, ko, Github) {
 
+    var github = new Github();
     var sha1 = ko.observable();
     var path = ko.observable();
+
     return {
         sha1: sha1,
         path: path,
+        content: ko.observable('loading...'),
         activate: function(routeOwner, routeRepo, routeSha1, routePath) {
+            var self = this;
+            
             sha1(routeSha1);
             path(routePath);
+
+            github.fetchRawFile(routeOwner, routeRepo, routeSha1).then(function(content){
+              self.content(content);
+            });
         }
     };
 });
