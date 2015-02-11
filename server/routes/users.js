efa7e5545e552d10985e0808dafb5fdc1b1c2600
Christopher Bennage <christopher@bennage.com>
2015-02-10 18:19:46 -0800

    basic express + github auth, not working though

---
+++ server/routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/:id', function(req, res, next) {
+  res.send('child ' + req.params.id);
+});
+
+module.exports = router;
