98b90bb851d5bdbdd094e8f2b9640bb1830f910e
Christopher Bennage <christopher@bennage.com>
2015-02-13 17:02:59 -0800

    attempting to make a production build

---
+++ build/production-build.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var changed = require('gulp-changed');
+var plumber = require('gulp-plumber');
+var to5 = require('gulp-6to5');
+var sourcemaps = require('gulp-sourcemaps');
+var paths = require('./paths');
+var compilerOptions = require('./6to5-options');
+var assign = Object.assign || require('object.assign');
+var minifyHTML = require('gulp-minify-html');
+var del = require('del');
+var vinylPaths = require('vinyl-paths');
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+
+gulp.task('stylus', function () {
+    gulp.src('./stylus/styles.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('./server/public/styles'));
+});
+
+gulp.task('clean', function() {
+  return gulp.src([paths.output])
+    .pipe(vinylPaths(del));
+});
+
+gulp.task('build-system', function () {
+  return gulp.src(paths.source)
+    .pipe(plumber())
+    .pipe(changed(paths.output, {extension: '.js'}))
+    .pipe(sourcemaps.init())
+    .pipe(to5(assign({}, compilerOptions, {modules:'system'})))
+    .pipe(sourcemaps.write({includeContent: false, sourceRoot: '/' + paths.root }))
+    .pipe(gulp.dest(paths.output));
+});
+
+gulp.task('build-html', function () {
+    var opts = {comments:true,spare:true};
+
+    return gulp.src(paths.html)
+        .pipe(changed(paths.output, {extension: '.html'}))
+        .pipe(minifyHTML(opts))
+        .pipe(gulp.dest(paths.output));
+});
+
+gulp.task('build', function(callback) {
+  return runSequence(
+    'clean',
+    ['build-system', 'build-html', 'stylus'],
+    callback
+  );
+});
