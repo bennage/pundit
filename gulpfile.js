37c7eb823cb4a3ccf83bd7f9c8d4a6d9df24a29c
Christopher Bennage <christopher@bennage.com>
2016-02-09 22:34:25 -0800

    add modernizr (kinda) prematurely too

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 const gulp = require('gulp');
+const modernizr = require('modernizr');
+const stream = require('stream');
 
 gulp.task('default', () => {
+
     gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css')
         .pipe(gulp.dest('./public/css'));
+
+    // modernizr.build({}, (result) => {
+    //     var s = new stream.Readable();
+    //     // s._read = function noop() { }; // redundant? see update below
+    //     s.push(result);
+    //     s.push(null);
+    //     s.pipe(gulp.dest('./public/js/modernizr.js'));
+    // });
 });
